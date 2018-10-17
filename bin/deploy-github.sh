19855ee20d656b2d8c7d0974f1b2b38d8d0f0f8f
chantep <541149506@qq.com>
2018-10-17 20:04:05 +0800

    sh update

--- bin/deploy-github.sh
+++ bin/deploy-github.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-npm run build
-git add .
-git ci -m "shell build..."
+# npm run build
+# git add .
+# git ci -m "shell build..."
 # git push github :gh-pages
 git subtree push --prefix dist github gh-pages
